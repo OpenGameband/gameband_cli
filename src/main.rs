d65070e9fb157f8758b1065d9296a088a30c2669
Zaprit <henry@vorax.org>
2022-12-13 15:53:08 +0000

    Moved logic to library

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,16 @@
-mod data;
+extern crate core;
 
-use std::time::{SystemTime, UNIX_EPOCH};
-
-fn pack_time(buf: &mut [u8], offset: usize, seconds: u64) {
-        buf[offset] = seconds as u8;
-        buf[offset + 1] = (seconds >> 8) as u8;
-        buf[offset + 2] = (seconds >> 16) as u8;
-        buf[offset + 3] = (seconds >> 24) as u8;
-}
+use oxidefurnace::gameband::open_gameband;
+use oxidefurnace::time::set_time;
 
 fn main() {
-    let api = hidapi::HidApi::new().unwrap();
-
-    let (vid, pid) = (0x2a90, 0x0021);
-    let device = api.open(vid, pid).unwrap();
-
-
-    let seconds = SystemTime::now().duration_since(UNIX_EPOCH).expect("It's 1969???").as_secs();
-    let mut buf: [u8;9] = [0;9];
-    buf[1] = 2;
-
-    pack_time(&mut buf, 5, seconds);
-
-    device.write(&buf).expect("something went wrong");
+    let gameband = open_gameband();
+    match gameband {
+        None => {
+            println!("Gameband not found");
+        }
+        Some(gb) => {
+            set_time(gb).expect("Failed to set time");
+        }
+    }
 }
